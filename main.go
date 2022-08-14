14c247733667761f08889bb744bf7cf3e4362796
lethehoa <tretrauchamcom1@gmail.com>
2022-08-14 18:55:56 +0700

    fix cannot run under crontab

--- main.go
+++ main.go
@@ -19,27 +19,34 @@ type vm_information struct {
 }
 
 func main() {
-	// var path string
+	var path string
 	_, err := os.Stat("log")
 	if os.IsNotExist(err) {
 		err = os.Mkdir("log", 0755)
 	}
 	create_off_list_fw()
-	// file_success, _ := os.OpenFile("log/log_success.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	// file_fail, _ := os.OpenFile("log/log_fail.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file_success, _ := os.OpenFile("log/log_success.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	file_fail, _ := os.OpenFile("log/log_fail.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
 	for i := 0; i < len(vm_list); i++ {
-		// if strings.Compare(strings.TrimSpace(vm_list[i].powerState), "poweredOff") == 0 {
-		// 	fmt.Println("Deleting ", vm_list[i].Full_path)
-		// 	path = strings.TrimSpace(vm_list[i].Full_path)
-		// 	time.Sleep(6 * time.Second)
-		// 	// _, err := exec.Command("govc", "vm.destroy", path).Output()
-		// 	if err == nil {
-		// 		write_log(file_success, path, "success")
-		// 	} else {
-		// 		write_log(file_fail, path, "fail")
-		// 	}
-		// }
-		fmt.Println(strings.TrimSpace(vm_list[i].Full_path), strings.TrimSpace(vm_list[i].powerState))
+		if strings.Compare(strings.TrimSpace(vm_list[i].powerState), "poweredOff") == 0 {
+			fmt.Println("Deleting ", vm_list[i].Full_path)
+			path = strings.TrimSpace(vm_list[i].Full_path)
+			time.Sleep(6 * time.Second)
+			// _, err := exec.Command("govc", "vm.destroy", path).Output()	//Delete fw - uncomment when needed
+			if err == nil {
+				write_log(file_success, path, "success")
+			} else {
+				write_log(file_fail, path, "fail")
+			}
+		}
+		out, _ := exec.Command("govc", "ls").Output()
+		file, _ := os.Create("test.txt")
+		_, err := file.Write([]byte(string(out)))
+		if err != nil {
+			fmt.Println("test")
+		}
+		// fmt.Println(strings.TrimSpace(vm_list[i].Full_path), strings.TrimSpace(vm_list[i].powerState))
 	}
 }
 
@@ -59,9 +66,10 @@ func write_log(fileName *os.File, vm_path string, key string) {
 
 func create_off_list_fw() {
 	out, _ := exec.Command("govc", "ls", "/VDC-Auto-Firewall/vm").Output()
+	result := string(out)
 	date_used := today.Format("02-01-2006")
 	file, _ := os.Create("log/off-list-" + date_used + ".log")
-	write_off_list_to_file(file, string(out))
+	write_off_list_to_file(file, result)
 }
 
 func write_off_list_to_file(fileName *os.File, vm_list_returned string) {
