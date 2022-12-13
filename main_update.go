36326053e09167bc9d608ed47eefcbb0e199c61f
root <root@awx.vietnix.vn>
2022-12-14 01:00:40 +0700

    update && optimal code

--- main_update.go
+++ main_update.go
@@ -32,7 +32,7 @@ func main() {
 		if strings.Compare(strings.TrimSpace(vm_list[i].powerState), "poweredOff") == 0 {
 			fmt.Println("Deleting ", vm_list[i].Full_path)
 			path = strings.TrimSpace(vm_list[i].Full_path)
-			time.Sleep(6 * time.Second)
+			time.Sleep(4 * time.Second)
 			_, err := exec.Command("govc", "vm.destroy", path).Output() //Delete fw - uncomment when needed
 			if err == nil {
 				write_log(file_success, path, "success")
@@ -42,7 +42,7 @@ func main() {
 		}
 		// fmt.Println(strings.TrimSpace(vm_list[i].Full_path), strings.TrimSpace(vm_list[i].powerState))
 	}
-  	create_fw()
+	//create_fw()
 }
 
 func write_log(fileName *os.File, vm_path string, key string) {
@@ -68,54 +68,21 @@ func create_off_list_fw() {
 }
 
 func write_off_list_to_file(fileName *os.File, vm_list_returned string) {
-	var s, vm_date_time string
+	var s string
 	scanner := bufio.NewScanner(strings.NewReader(vm_list_returned))
-
 	for scanner.Scan() {
 		full_path := scanner.Text()
 		arr := strings.Split(full_path, "/")
 		s = arr[3]
-		m, _ := regexp.MatchString(`(?m)(\d{1,4}([.\-\/])\d{1,2}([.\-\/])\d{1,4})`, s) // get date
+		m, _ := regexp.MatchString(`((?m)(\d{1,4}([.\-\/])\d{1,2}([.\-\/])\d{2,4}))$`, s) // get date
 		if strings.Contains(s, "-off-") && m || strings.Contains(s, "-reinstall-") && m {
-			vm_date_time = return_date_time(s) //check if vm_path end with date
-			if vm_date_time != "not_valid" {
-				vm_list = append(vm_list, return_a_struct_from_vm_info(full_path))
-				_, err := fileName.Write([]byte(s + "\n"))
-				if err != nil {
-					log.Fatal(err)
-				}
+			vm_list = append(vm_list, return_a_struct_from_vm_info(full_path))
+			_, err := fileName.Write([]byte(s + "\n"))
+			if err != nil {
+				log.Fatal(err)
 			}
 		}
 	}
-}
-
-func return_date_time(vm_path string) string {
-	var re = regexp.MustCompile(`(?m)(\d{1,4}([.\-\/])\d{1,2}([.\-\/])\d{1,4})`) //return date-time
-	var str_final = re.FindString(vm_path)
-	str_final_array := strings.Split(str_final, "-")
-	if !strings.HasSuffix(vm_path, str_final) {
-		return "not_valid"
-	}
-	if strings.Contains(str_final, "-22") || strings.Contains(str_final, "-2") { //fix pattern
-		str_final_array[2] = "2022"
-		str_final = strings.Join(str_final_array[:], "-")
-		return str_final
-	}
-	return str_final
-
-}
-
-func compare_date_time_with_current(date string) bool {
-	today := time.Now()
-	t, err := time.Parse("2-1-2006", date)
-	if err != nil {
-		fmt.Println(err)
-	}
-	if today.YearDay()-t.YearDay() > 2 {
-		return true // xoa
-	}
-	return false // khong xoa
-
 }
 
 func return_a_struct_from_vm_info(full_path string) vm_information {
